ce3c6564e5c8753798f7313074422c7128406e7a
rnqhstjr12 <rnqhstjr12@gmail.com>
2023-12-21 17:58:16 +0900

    boot_3

--- src/main/java/me/ezen/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/ezen/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.ezen.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.ezen.springbootdeveloper.domain.Article;
 import me.ezen.springbootdeveloper.dto.AddArticleRequest;
+import me.ezen.springbootdeveloper.dto.ArticleResponse;
 import me.ezen.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +23,16 @@ public class BlogApiController {
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findALlArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/ezen/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/ezen/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.ezen.springbootdeveloper.dto.AddArticleRequest;
 import me.ezen.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -13,5 +15,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
